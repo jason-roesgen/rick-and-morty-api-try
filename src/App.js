390c14061984e625ddca299029b14bf485749696
jason-roesgen <jason.roesgen@gmail.com>
2022-07-27 23:21:24 +0200

    added routing to main and subpages

--- src/App.js
+++ src/App.js
@@ -1,35 +1,46 @@
-import './App.css';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import 'bootstrap/dist/css/bootstrap.min.css';
-// import EpisodesList from './pages/EpisodesList'
-// import CharactersList from './pages/CharactersList'
-
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import EpisodesList from "./pages/EpisodesList";
+import CharactersList from "./pages/CharactersList";
+import Home from "./pages/Home";
 
 // first todos:
 // 1. add top bar
 // 2. add search bar
-// 3. adjust search bar with magnifying glass	icon 
+// 3. adjust search bar with magnifying glass	icon
 // 4. add 2 pages (for characters & episodes)
 // 5. add routing (maybe put both pages as buttons on the left of the top bar?)
 function App() {
+  let component;
+  switch (window.location.pathname) {
+    case "/characters":
+      component = <CharactersList />;
+      break;
+    case "/episodes":
+      component = <EpisodesList />;
+      break;
+    default:
+      component = <Home />;
+  }
   return (
     <>
-    <Navbar bg="light" expand="sm">
-      <Container>
-        <Navbar.Brand href='#'>Coding Challenge</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#episodes">Episodes</Nav.Link>
-            <Nav.Link href="#characters">Characters</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-      {/* <EpisodesList /> 
-      <CharactersList /> */}
+      <Navbar bg="light" expand="sm">
+        <Container>
+          <Navbar.Brand href="/home">Coding Challenge</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/episodes">Episodes</Nav.Link>
+              <Nav.Link href="/characters">Characters</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      {component}
     </>
   );
 }
