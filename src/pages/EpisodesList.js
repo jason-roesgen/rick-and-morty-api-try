080c8a4d99edce0df5375c3a4abf5aba7c341525
jason-roesgen <jason.roesgen@gmail.com>
2022-07-29 00:52:45 +0200

    added mock data into EpisodesList page for testing

--- src/pages/EpisodesList.js
+++ src/pages/EpisodesList.js
@@ -1,3 +1,15 @@
+import Data from "../mock-data.json"
+
 export default function EpisodesList() {
-  return <h1>that is from the episodes page</h1>;
+  return (
+    Data.map((post) => (
+      <div key={post.id}>
+        <p>{post.title}</p>
+        <p>{post.author}</p>
+      </div>
+    )),
+
+    <h1>that is from the episodes page</h1>
+  
+  );
 }
