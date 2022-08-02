108c2bfd8f5829cce80f22116b1ff5ecd265dde2
jason-roesgen <jason.roesgen@gmail.com>
2022-08-03 00:59:14 +0200

    fetch and display fake API for testing

--- src/pages/EpisodesList.js
+++ src/pages/EpisodesList.js
@@ -1,15 +1,8 @@
-import Data from "../mock-data.json"
+
+// https://rickandmortyapi.com/api/episode
 
 export default function EpisodesList() {
   return (
-    Data.map((post) => (
-      <div key={post.id}>
-        <p>{post.title}</p>
-        <p>{post.author}</p>
-      </div>
-    )),
-
     <h1>that is from the episodes page</h1>
-  
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,27 @@
+import "./Home.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 export default function Home() {
-  return <h1>that is just the main page</h1>;
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const { data } = await axios.get("https://jsonplaceholder.typicode.com/posts");
+    setPosts(data);
+  };
+
+  return (
+    <div className="list">
+      {posts.map((post) => (
+        <div key={post.id}>
+          <h5>{post.title}</h5>
+          <p>{post.body}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
